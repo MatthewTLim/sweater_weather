8e35a0f6f4d71c5c600b83f016b58afe6c02838f
Matthew Lim <125940571+MatthewTLim@users.noreply.github.com>
2023-09-24 15:10:21 -0700

    test: add forecast facaade tests

---
+++ spec/facades/forecast_facade_spec.rb
@@ -0,0 +1,18 @@
+require "rails_helper"
+
+RSpec.describe "ForecastFacade" do
+  describe "#class_methods" do
+    it "gets forecast using latitude and longitude" do
+      lat = "45.51179"
+      lng = "-122.67563"
+
+      forecast_facade = ForecastFacade.new
+      receive_forecast = forecast_facade.receive_forecast(lat, lng)
+
+      expect(receive_forecast).to be_a(Forecast)
+      expect(receive_forecast.current_weather).to be_a(Hash)
+      expect(receive_forecast.daily_weather).to be_an(Array)
+      expect(receive_forecast.hourly_weather).to be_an(Array)
+    end
+  end
+end
